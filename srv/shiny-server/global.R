2345f65ad90a302beec978fe755539cc4b5e3e50
Stephanie Eaneff <steph.eaneff@gmail.com>
2020-05-28 15:55:27 -0700

    move data subdirectory

--- srv/shiny-server/global.R
+++ srv/shiny-server/global.R
@@ -3,10 +3,10 @@
 ######################################################################
 
 ## read in full list  of model outputs, model runs, and locations
-model_outputs <- readRDS(here("data", "model_outputs.RDS"))
-models <- read.delim(here("data", "models.txt"), stringsAsFactors = FALSE)
-model_runs <- read.delim(here("data", "model_runs.txt"), stringsAsFactors = FALSE)
-locations <- read.delim(here("data", "locations.txt"), stringsAsFactors = FALSE)
+model_outputs <- readRDS("data/model_outputs.RDS")
+models <- read.delim("data/models.txt", stringsAsFactors = FALSE)
+model_runs <- read.delim("data/model_runs.txt", stringsAsFactors = FALSE)
+locations <- read.delim("data/locations.txt", stringsAsFactors = FALSE)
 output_options <- unique(model_outputs$output_name)
 
 ## locations for which you can view data, grouped separately by countries and US states/territories/intermediate areas
@@ -22,45 +22,6 @@ outputs_agg <- list("Healthcare demand" = c("Hospital beds needed per day",
                                            "Ventilators needed per day"),
                     "Fatalities" = c("Cumulative fatalities",
                                      "Fatalities per day"))
-
-####################################################################################
-## Process cumulative fatality estimates to also estimate fatalities per day #######
-####################################################################################
-# 
-# undoCumSum <- function(x) {
-#   c(NA, diff(x))
-# }
-# ## sanity check:
-# #undoCumSum(cumsum(seq(1:10)))
-# 
-# ## exclude IHME and GLEAM data because they report daily fatalities directly
-# model_outputs_cumfatal <- model_outputs[which(model_outputs$output_name == "Cumulative fatalities" & !model_outputs$model_run_id %in% model_runs$model_run_id[which(model_runs$model_id %in% c(1, 10))]),]
-# model_outputs_cumfatal <- model_outputs[which(model_outputs$output_name == "Cumulative fatalities" & !model_outputs$model_run_id %in% model_runs$model_run_id[which(model_runs$model_id %in% c(1, 10))]),]
-# 
-# model_outputs_cumfatal <- model_outputs_cumfatal[order(model_outputs_cumfatal$date, decreasing = FALSE),]
-# 
-# model_outputs_cumfatal <- model_outputs_cumfatal %>%
-#   group_by(model_run_id, output_id, output_name, location, value_type) %>%
-#   mutate(daily_fatalities =  undoCumSum(value))
-# 
-# ## pause here -- some models do weird things to estimate cumulative fatalities
-# ## and intentionally don't report fatalities per day
-# ## in some cases, cumulative fatalities aren't monotonically increasing, which is nonsensical with the exception
-# ## of maybe assuming case definitions or reporting has changed, essentially, data/surveillance artificts
-# ## to deal with this for now, don't calculate fatalities per day for any model_run_ids that ever shows such a decrease
-# exclude <- model_outputs_cumfatal[which(model_outputs_cumfatal$daily_fatalities < 0),]$model_run_id
-# model_outputs_cumfatal <- model_outputs_cumfatal[-which(model_outputs_cumfatal$model_run_id %in% exclude),]
-# 
-# model_outputs <- rbind.data.frame(model_outputs,
-#                                   cbind.data.frame(
-#                                     model_run_id = model_outputs_cumfatal$model_run_id,
-#                                     output_id = model_outputs_cumfatal$output_id,
-#                                     output_name = "Fatalities per day",
-#                                     date = model_outputs_cumfatal$date,
-#                                     location = model_outputs_cumfatal$location,
-#                                     value_type = model_outputs_cumfatal$value_type,
-#                                     value = model_outputs_cumfatal$daily_fatalities,
-#                                     notes = "calculated based on reported estimates of cumulative fatalities"))
 
 ######################################################################
 ## Calculate derivative datasets, lists, etc  ########################
